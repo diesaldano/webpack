fd8f86e7b80f3fb293c2d61605c8ea005c6d278f
Diego <diesaldano@gmail.com>
2021-09-06 23:44:41 -0300

    feat add variable enviroment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'production', // LE INDICO EL MODO EXPLICITAMENTE
@@ -88,8 +88,8 @@ module.exports = {
                 to: "assets/images"
               }
             ]
-          }),
-
+        }),
+        new Dotenv()
     ],
     optimization: {
         minimize:true,
